848efabc0d33c5727e9df7a029779b57d818220b
Hampus Lundqvist <haslun16@edu.vilhelmina.se>
2018-04-06 22:34:23 +0200

    (Tried) adding top

---
+++ commands/top.js
@@ -0,0 +1,66 @@
+class top {
+  constructor (thot) {
+    this.thot = thot
+
+    this.thot.register({ command: 'v!top', usage: '<Page>', description: 'Shows the poems leaderboard.', callback: this.handle.bind(this), admin: false })
+  }
+
+  async handle (message) {
+    let poems = this.thot.storage.poems
+    if (!poems) { return }
+
+    let page = 0
+
+    if (!isNaN(parseInt(message.content.split(' ')[1]))) { page = parseInt(message.content.split(' ')[1]) }
+
+    let leaderboard = []
+
+    Object.keys(poems).forEach(key => {
+      leaderboard.push({
+        userid: key,
+        poems: poems[key]
+      })
+    })
+
+    leaderboard.sort((a, b) => {
+      if (a.poems < b.poems) return -1
+      if (a.poems > b.poems) return 1
+      return 0
+    })
+
+    if (page > 0) {
+      leaderboard = leaderboard.slice(10 * page - 1, 10 * page + 9)
+    } else {
+      leaderboard = leaderboard.slice(0, 9)
+    }
+
+    if (leaderboard.length === 0) {
+      this.thot.send(message.channel, {
+        title: `Poem Leaderboard`,
+        description: `Page ${page} is out of range.`,
+        color: 15347007
+      })
+      message.delete()
+      return
+    }
+
+    let topStr = ''
+    let i = 0
+
+    await leaderboard.forEach(async uid => {
+      let user = message.guild.members.get(uid.userid)
+      if (!user) { user = await message.guild.fetchMember(uid.userid) }
+
+      topStr += `**[${i * (page + 1)}]** ${user.username} - ${uid.poemd} ${poems === 1 ? 'poem' : 'poems'}`
+    })
+
+    this.thot.send(message.channel, {
+      title: `Poem Leaderboard`,
+      description: topStr,
+      color: 431075
+    })
+    message.delete()
+  }
+}
+
+module.exports = top
