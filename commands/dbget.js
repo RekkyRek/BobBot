6ffbe53c342d8f60ec80302328f3229c59311e28
Hampus Lundqvist <haslun16@edu.vilhelmina.se>
2018-04-14 18:25:03 +0200

    Added some debug commands

---
+++ commands/dbget.js
@@ -0,0 +1,22 @@
+class give {
+  constructor (thot) {
+    this.thot = thot
+
+    this.thot.register({ command: 'v!dbget', usage: '<Table> <Key>', description: 'Debug command, get a value from the database.', callback: this.handle.bind(this), admin: true })
+  }
+
+  async handle (message) {
+    if (!this.thot.checkPerms(message)) { return }
+
+    let table = message.content.split(' ')[1]
+    let key = message.content.split(' ')[2]
+    if (!table || !key) { return }
+
+    let data = this.thot.get(table, key)
+
+    message.channel.send(`db.${table}.${key}:\`\`\`JSON\n${JSON.stringify(data, null, 2)}\n\`\`\``)
+    message.delete()
+  }
+}
+
+module.exports = give
---
+++ commands/dbset.js
@@ -0,0 +1,23 @@
+class give {
+  constructor (thot) {
+    this.thot = thot
+
+    this.thot.register({ command: 'v!dbset', usage: '<Table> <Key> <Value>', description: 'Debug command, set a value in the database.', callback: this.handle.bind(this), admin: true })
+  }
+
+  async handle (message) {
+    if (!this.thot.checkPerms(message)) { return }
+
+    let table = message.content.split(' ')[1]
+    let key = message.content.split(' ')[2]
+    let value = message.content.split(' ')[3]
+    if (!table || !key || !value) { return }
+
+    this.thot.set(table, key, value)
+
+    message.channel.send(`Successfully set db.${table}.${key} to ${value}`)
+    message.delete()
+  }
+}
+
+module.exports = give
